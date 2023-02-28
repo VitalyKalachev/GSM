9282a1465b9f6b525446f25ea7e459da729dd920
VitalyKalachev <kalachev-1993@ya.ru>
2023-03-01 00:15:36 +0300

    IntelliJ IDEA

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,14 @@ public class Main {
     public static void main(String[] args) {
         int balance = 250;
         int replenishment = 1000;
-        int bonus = 12;
+        int bonus;
         if (replenishment >= 1000) {
+            System.out.println((balance + replenishment) / 100);
+            bonus = 12;
             System.out.println(balance + replenishment + bonus);
         } else {
             System.out.println(balance + replenishment);
+            bonus = 0;
         }
     }
 }
